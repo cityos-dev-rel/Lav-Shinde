26ca3a02deffe2b96233ff9af5effe44ed9e264c
Lav Shinde <shindelav@gmail.com>
2023-02-12 13:13:42 +0900

    Add Test

--- tests/Feature/VideoStoreTest.php
+++ tests/Feature/VideoStoreTest.php
@@ -2,11 +2,14 @@
 
 namespace Tests\Feature;
 
+use App\Models\Video;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
+use Illuminate\Support\Str;
 
 class VideoStoreTest extends TestCase
 {
@@ -27,5 +30,20 @@ class VideoStoreTest extends TestCase
         ]);
 
         $response->assertBadRequest();
+    }
+
+    public function testVideoStoreWithFileAlreadyExists()
+    {
+        Storage::fake('s3');
+        $file = UploadedFile::fake()->create('test.mp4', 100);
+        Video::factory()->create([
+            'title' => $file->getClientOriginalName(),
+        ]);
+
+        $response = $this->postJson('/files', [
+            'data' => $file,
+        ]);
+
+        $response->assertStatus(409);
     }
 }
